8b23871eec0d5570d85520a54486629ad06d654d
vince-bull <vincebull@hotmail.fr>
2026-02-14 16:12:09 -0500

    Partie 3.3

--- rx_poc.sh
+++ rx_poc.sh
@@ -1,2 +1,17 @@
 #!/usr/bin/bash
-curl wttr.in/$city -o weather_report.txt
+city=Casablanca
+# curl -s wttr.in/$city?T --output weather_report.txt
+
+#To extract Current Temperature
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
+echo "The current Temperature of $city: $obs_temp"
+
+# To extract the forecast tempearature for noon tomorrow
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+
+# set the time-zone environment variable for Casablanca
+TZ='Morocco/Casablanca'
+
+year=$(date +%Y)
+echo $year
